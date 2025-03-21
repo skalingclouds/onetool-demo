a799c4891f8c97fa06771423979109f7bfd745b3
Paul K. <paul@paulkr.com>
2025-03-21 12:56:50 -0400

    chore: upgrade packages

--- app/(preview)/api/authkit/route.ts
+++ app/(preview)/api/authkit/route.ts
@@ -1,23 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { AuthKitToken } from "@picahq/authkit-node";
 
-
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type",
 };
 
-// Handle OPTIONS requests
 export async function OPTIONS(req: NextRequest) {
   return NextResponse.json({}, { headers: corsHeaders });
 }
 
-
 export async function POST(req: NextRequest) {
-
   const authKitToken = new AuthKitToken(process.env.PICA_SECRET_KEY as string);
-  
+
   const token = await authKitToken.create({
     identity: "user_123", // a meaningful identifier (i.e., userId, teamId or organizationId)
     identityType: "user" // can be either user, team or organization
